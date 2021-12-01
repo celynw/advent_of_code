943557a1afa07f3d23768468ce3b765a06230c59
Celyn Walters <3299161+celynw@users.noreply.github.com>
2021-12-01 14:35:24 +0000

    Solve 2021/01

---
+++ 2021/solvers/1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from kellog import info, warning, error, debug
+
+import utils
+
+# ==================================================================================================
+def solve():
+	lines = utils.read_puzzle_input(Path(__file__))
+	debug(f"Number of depth measurements: {len(lines)}")
+
+	prevDepth = None
+	increased = 0
+	decreased = 0
+	for line in lines:
+		currDepth = int(line.strip())
+		if prevDepth is None:
+			prevDepth = currDepth
+			continue
+		if currDepth > prevDepth:
+			increased += 1
+		elif currDepth < prevDepth:
+			decreased += 1
+		prevDepth = currDepth
+
+	info(f"increased: {increased}")
+	info(f"decreased: {decreased}")
+
+
+# ==================================================================================================
+if __name__ == "__main__":
+	solve()
