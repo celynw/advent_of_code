195c77bcd539f61de0da6f677c4dca5ba5157e4b
Celyn Walters <3299161+celynw@users.noreply.github.com>
2021-12-01 14:53:03 +0000

    Fix issue in `utils.py`

--- 2021/utils.py
+++ 2021/utils.py
@@ -2,11 +2,11 @@
 from pathlib import Path
 
 # ==================================================================================================
-def get_puzzle_path(path: Path):
-	return path.parent.parent / "puzzle_input" / "1.txt"
+def get_puzzle_path(path: Path, index: str):
+	return path.parent.parent / "puzzle_input" / f"{index}.txt"
 
 # ==================================================================================================
 def read_puzzle_input(path: Path):
-	path = get_puzzle_path(path)
+	path = get_puzzle_path(path, path.stem)
 	with open(path, "r") as file:
 		return file.read().splitlines()
