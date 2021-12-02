28b8c34351a9deeb6e404f76d0673ad9ccb93b46
Celyn Walters <3299161+celynw@users.noreply.github.com>
2021-12-02 15:46:46 +0000

    Solve 2021/03

---
+++ 2021/solvers/3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from kellog import info, warning, error, debug
+
+import utils
+
+# ==================================================================================================
+def solve():
+	lines = utils.read_puzzle_input(Path(__file__))
+	debug(f"Number of commands: {len(lines)}")
+	distance = 0
+	depth = 0
+	for line in lines:
+		direction, amount = line.split(" ")
+		if direction == "forward":
+			distance += int(amount)
+		elif direction == "down":
+			depth += int(amount)
+		elif direction == "up":
+			depth -= int(amount)
+
+	info(f"Final distance: {distance}")
+	info(f"Final depth: {depth}")
+	info(f"Multiplied: {distance * depth}")
+
+
+# ==================================================================================================
+if __name__ == "__main__":
+	solve()
