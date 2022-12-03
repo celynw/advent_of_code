903b7e3f9e3f42a5526c850510dc35ea03b6a627
Celyn Walters <3299161+celynw@users.noreply.github.com>
2022-12-03 14:58:40 +0000

    Solve 2022/06

---
+++ 2022/solvers/6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from rich import print, inspect
+
+# ==================================================================================================
+def solve(lines: list[str]) -> int:
+	priority = 0
+	group = []
+	for line in lines:
+		group.append(line)
+		if len(group) == 3:
+			badge = list(set(group[0]) & set(group[1]) & set(group[2]))[0]
+
+			if badge == badge.lower():
+				priority += ord(badge) - ord("a") + 1
+			elif badge == badge.upper():
+				priority += ord(badge) - ord("A") + 27
+			group = []
+	print(priority)
+
+	return priority
