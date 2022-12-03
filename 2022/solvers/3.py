2ec5bbdd8f58c6209e116d342d0b3b89e7579d51
Celyn Walters <3299161+celynw@users.noreply.github.com>
2022-12-03 14:31:40 +0000

    Solve 2022/03

---
+++ 2022/solvers/3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from rich import print, inspect
+
+# ==================================================================================================
+def solve(lines: list[str]) -> int:
+	mapping = {
+		"X": "A",
+		"Y": "B",
+		"Z": "C",
+	}
+	possibilities = list(mapping.values())
+
+	score = 0
+	for line in lines:
+		opponent, you = line.split(" ")
+
+		# Score for what you played
+		you = mapping[you]
+		score += possibilities.index(you) + 1
+
+		# Score for the result
+		result = (possibilities.index(you) - possibilities.index(opponent)) % len(possibilities)
+		match result:
+			case _ if result == 0: # draw
+				score += 3
+			case _ if result == 1: # win
+				score += 6
+			# case _: # lose
+	print(score)
+
+	return score
