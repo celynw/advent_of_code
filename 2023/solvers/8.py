89a725c1b98f7173919d165ad11b739ce3e78c60
Celyn Walters <3299161+celynw@users.noreply.github.com>
2023-12-04 20:46:11 +0000

    Solve 2023/08

---
+++ 2023/solvers/8.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import colored_traceback.auto  # noqa: F401
+
+
+# ======================================================================================================================
+def solve(lines: list[str]) -> int:
+	"""Print and return the solution. Called by ../solve.py."""
+	won = {}
+	total = 0
+	for l, line in enumerate(lines):
+		winning, have = ([int(x) for x in item.split()] for item in line.split(": ")[1].split(" | "))
+
+		score = 0
+		for h in have:
+			if h in winning:
+				score += 1
+
+		if score:
+			won[l] = [l + r + 1 for r in range(score)]
+		else:
+			total += 1  # This card
+
+	for w in won:
+		total += accumulate_cards(won, w)
+
+	print(total)  # noqa: T201
+
+	return total
+
+
+# ======================================================================================================================
+def accumulate_cards(won: dict[int, list[int]], index: int, indent: str = "") -> int:
+	"""Recurse and count the total number of won cards."""
+	connected = won.get(index, [])
+	total = 1  # This card
+
+	for c in connected:
+		total += accumulate_cards(won, c, indent + "  ")
+
+	return total
