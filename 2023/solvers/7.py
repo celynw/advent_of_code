d28fed5a4044e70837640680a845ba025ee4a715
Celyn Walters <3299161+celynw@users.noreply.github.com>
2023-12-04 08:55:30 +0000

    Solve 2023/07

---
+++ 2023/solvers/7.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import colored_traceback.auto  # noqa: F401
+
+
+# ======================================================================================================================
+def solve(lines: list[str]) -> int:
+	"""Print and return the solution. Called by ../solve.py."""
+	scores = []
+	for line in lines:
+		winning, have = ([int(x) for x in item.split()] for item in line.split(": ")[1].split(" | "))
+
+		score = 0
+		for h in have:
+			if h in winning:
+				score = score * 2 if score else 1
+		scores.append(score)
+
+	print(sum(scores))
+
+	return sum(scores)
