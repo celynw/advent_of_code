9414141e33a65303766969114b2ce96abcf85d39
Celyn Walters <3299161+celynw@users.noreply.github.com>
2023-12-05 23:56:40 +0000

    Solve 2023/09

---
+++ 2023/solvers/9.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import colored_traceback.auto  # noqa: F401
+
+
+# ======================================================================================================================
+def solve(lines: list[str]) -> int:
+	"""Print and return the solution. Called by ../solve.py."""
+	targets = [int(s) for s in lines.pop(0).split(" ")[1:]]
+	maps = parse(lines)
+
+	for mapping in maps.values():
+		for s, seed in enumerate(targets):
+			for l in mapping:
+				dest_start, src_start, length = l
+				if seed in range(src_start, src_start + length + 1):
+					targets[s] = dest_start + (seed - src_start)
+					break
+				targets[s] = seed
+
+	print(min(targets))  # noqa: T201
+
+	return min(targets)
+
+
+# ======================================================================================================================
+def parse(lines: list[str]) -> dict[str, list[list[int]]]:
+	"""Extract maps from input lines."""
+	maps = {}
+	name = ""
+	these_maps = []
+	for line in lines:
+		if line == "":
+			continue
+
+		if ":" in line:
+			if name != "":
+				maps[name] = these_maps
+			name, _ = line.split(" ")
+			these_maps = []
+		else:
+			these_maps.append([int(n) for n in line.split(" ")])
+
+	if name not in maps:
+		maps[name] = these_maps
+
+	return maps
