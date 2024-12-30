49a8af53d475319f34bfbcc178cb2684a6ef3647
Celyn Walters <3299161+celynw@users.noreply.github.com>
2024-12-30 16:39:27 +0000

    Solve 2024/01

---
+++ 2024/solvers/1.py
@@ -0,0 +1,19 @@
+def solve(lines: list[str]) -> int:
+	"""
+	Print and return the solution. Called by ../solve.py.
+
+	Parameters
+	----------
+	lines
+		Puzzle input, as a list of line strings
+
+	Returns
+	-------
+		Puzzle solution
+	"""
+	pairs = [line.split("   ") for line in lines]
+	lefts, rights = zip(*pairs, strict=False)
+	lefts = sorted(lefts)
+	rights = sorted(rights)
+
+	return sum(abs(int(left) - int(right)) for left, right in zip(lefts, rights, strict=False))
